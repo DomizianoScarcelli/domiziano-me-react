1b048f3d5b1d7f6304ac44f9cc1cb86e1294a8c6
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-02-12 12:44:29 +0100

    Rearranging project container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,14 +1,15 @@
 import React from "react"
 import styles from "./Container.module.css"
-
+import colors from "../../colors"
 const Container = (props) => {
-	const titleStyle = {
-		color: props.titleColor,
-	}
-
 	return (
-		<div className={styles.container} id={`${props.title.toLowerCase()}`}>
-			<div className={styles.title} style={titleStyle}>
+		<div className={styles.container} id={`${props.title.toLowerCase()}`} style={{ backgroundColor: colors.foregroundLight, color: colors.background }}>
+			<div
+				className={styles.title}
+				style={{
+					color: props.titleColor,
+				}}
+			>
 				{props.title}
 			</div>
 			{props.children}
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,11 +1,9 @@
 import React from "react"
 import styles from "./Project.module.css"
 import { motion } from "framer-motion"
-import { useState } from "react"
 import { useMediaQuery } from "../../hooks/useMediaQuery"
 import colors from "../../colors"
 const Project = (props) => {
-	const [isHover, setHover] = useState(false)
 	const isSmall = useMediaQuery("(max-width: 1079px)")
 
 	const imageUrl = `/project-images/${props.image}`
@@ -20,38 +18,26 @@ const Project = (props) => {
 	}
 
 	return (
-		<motion.div
-			whileHover={{ scale: 1.1 }}
-			transition={{ stiffness: 200 }}
-			onHoverStart={() => {
-				setHover(true)
-			}}
-			onHoverEnd={() => {
-				setHover(false)
-			}}
-			className={styles.container}
-			style={colorStyle}
-		>
+		<div className={styles.container} style={colorStyle}>
 			<div className={styles.image} style={imageStyle}></div>
 			<div className={styles.textContainer}>
 				<div className={styles.title}>{props.title}</div>
 				<div className={styles.content}> {props.content}</div>
-				{(isHover || isSmall) && (
-					<motion.div className={styles.links}>
-						{props.github && (
-							<a className={styles.github} href={props.github} target="_blank" rel="noreferrer">
-								{" "}
-							</a>
-						)}
-						{props.demo && (
-							<a className={styles.demo} href={props.demo} target="_blank" rel="noreferrer">
-								{" "}
-							</a>
-						)}
-					</motion.div>
-				)}
+
+				<div className={styles.links}>
+					{props.github && (
+						<a className={styles.github} href={props.github} target="_blank" rel="noreferrer">
+							{" "}
+						</a>
+					)}
+					{props.demo && (
+						<a className={styles.demo} href={props.demo} target="_blank" rel="noreferrer">
+							{" "}
+						</a>
+					)}
+				</div>
 			</div>
-		</motion.div>
+		</div>
 	)
 }
 
