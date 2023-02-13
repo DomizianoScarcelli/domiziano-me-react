e667ef46a7b6b42d2ba72ec35d5ca40787223322
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-02-13 03:26:10 +0100

    Added link to github contributions

--- src/components/Contributions/Contributions.js
+++ src/components/Contributions/Contributions.js
@@ -7,22 +7,45 @@ const Contributions = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.line} style={{ backgroundColor: colors.foregroundMedium }}></div>
-			<ContributionPanel repository={"DamascenoRafael/reminders-menubar"} link={""} title={"Adding possibility to specify due date and time with natural language"} number={"105"} />
-			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Allowing users to choose a reddit thread instead of being randomized"} number={"238"} />
-			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Option to filter the comments by length (min and max number of characters)"} number={"261"} />
-			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Added a .env variable to change the save location of the final video"} number={"333"} />
+			<ContributionPanel
+				repository={"DamascenoRafael/reminders-menubar"}
+				link={"https://github.com/DamascenoRafael/reminders-menubar"}
+				title={"Adding possibility to specify due date and time with natural language"}
+				number={"105"}
+			/>
+			<ContributionPanel
+				repository={"elebumm/RedditVideoMakerBot"}
+				link={"https://github.com/elebumm/RedditVideoMakerBot/pull/238"}
+				title={"Allowing users to choose a reddit thread instead of being randomized"}
+				number={"238"}
+			/>
+			<ContributionPanel
+				repository={"elebumm/RedditVideoMakerBot"}
+				link={"https://github.com/elebumm/RedditVideoMakerBot/pull/261"}
+				title={"Option to filter the comments by length (min and max number of characters)"}
+				number={"261"}
+			/>
+			<ContributionPanel
+				repository={"elebumm/RedditVideoMakerBot"}
+				link={"https://github.com/elebumm/RedditVideoMakerBot/pull/333/"}
+				title={"Added a .env variable to change the save location of the final video"}
+				number={"333"}
+			/>
 		</div>
 	)
 }
 
 const ContributionPanel = ({ repository, title, link, number }) => {
+	const openPullRequest = (link) => {
+		window.open(link, "_blank")
+	}
 	return (
 		<div className={styles.panel} style={{ backgroundColor: colors.foregroundMedium }}>
-			<div href={link} className={styles.top} style={{ backgroundColor: colors.foregroundDark, borderColor: colors.foregroundMedium }}>
+			<div href={link} className={styles.top} style={{ backgroundColor: colors.foregroundDark, borderColor: colors.foregroundMedium }} onClick={() => openPullRequest(link)}>
 				<p>{repository}</p>
 			</div>
 			<p>
-				{title} <span>{`#${number}`}</span>
+				{title} <span onClick={() => openPullRequest(link)}>{`#${number}`}</span>
 			</p>
 			<div className={styles.merged}>
 				<img src={icons.gitMerge} alt="merged-icon" />
