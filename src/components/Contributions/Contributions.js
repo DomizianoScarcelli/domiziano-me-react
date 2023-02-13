e5dbc2bf7a760a6fc951699ce8f908d0f6724b96
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-02-13 03:14:43 +0100

    Changed project images

--- src/components/Contributions/Contributions.js
+++ src/components/Contributions/Contributions.js
@@ -8,9 +8,9 @@ const Contributions = () => {
 		<div className={styles.container}>
 			<div className={styles.line} style={{ backgroundColor: colors.foregroundMedium }}></div>
 			<ContributionPanel repository={"DamascenoRafael/reminders-menubar"} link={""} title={"Adding possibility to specify due date and time with natural language"} number={"105"} />
-			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Allowing users to choose a reddit thread instead of being randomized"} number={"238"}/>
-			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Option to filter the comments by length (min and max number of characters)"} number={"261"}/>
-			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Added a .env variable to change the save location of the final video"} number={"333"}/>
+			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Allowing users to choose a reddit thread instead of being randomized"} number={"238"} />
+			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Option to filter the comments by length (min and max number of characters)"} number={"261"} />
+			<ContributionPanel repository={"elebumm/RedditVideoMakerBot"} link={""} title={"Added a .env variable to change the save location of the final video"} number={"333"} />
 		</div>
 	)
 }
@@ -18,10 +18,12 @@ const Contributions = () => {
 const ContributionPanel = ({ repository, title, link, number }) => {
 	return (
 		<div className={styles.panel} style={{ backgroundColor: colors.foregroundMedium }}>
-			<div className={styles.top} style={{ backgroundColor: colors.foregroundDark, borderColor: colors.foregroundMedium }}>
+			<div href={link} className={styles.top} style={{ backgroundColor: colors.foregroundDark, borderColor: colors.foregroundMedium }}>
 				<p>{repository}</p>
 			</div>
-			<p>{title} <span>{`#${number}`}</span></p>
+			<p>
+				{title} <span>{`#${number}`}</span>
+			</p>
 			<div className={styles.merged}>
 				<img src={icons.gitMerge} alt="merged-icon" />
 				<p>Merged</p>
