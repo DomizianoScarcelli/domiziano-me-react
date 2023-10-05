3c82c839c41f59b930be75a3a064c7a2c9c29946
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-10-05 19:04:23 +0200

    Added netlify forms and protected email

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,21 +5,21 @@ import colors from "../../colors"
 const Contactform = () => {
 	return (
 		<div className={styles.main_container}>
-			<form action="https://formsubmit.co/scarcelli.domiziano@gmail.com" method="POST" name="contactModule" className={styles.container}>
+			<form action="https://formsubmit.co/4e698c3eb254383c5a9a0ddd66b504b3" method="POST" name="contactModule" className={styles.container} netlify>
 				{/* <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html"> */}
 				<input type="hidden" name="_autoresponse" value="Thanks for contacting me, I will answer ASAP!" />
 
 				<div className={styles.field}>
 					<div className={styles.title}>Name.</div>
-					<input type="text" name="name" className={styles.input} style={{ backgroundColor: colors.background }} />
+					<input type="text" name="name" className={styles.input} style={{ backgroundColor: colors.background }} required />
 				</div>
 				<div className={styles.field}>
 					<div className={styles.title}>Email.</div>
-					<input type="email" name="email" className={styles.input} style={{ backgroundColor: colors.background }} />
+					<input type="email" name="email" className={styles.input} style={{ backgroundColor: colors.background }} required />
 				</div>
 				<div className={styles.field + " " + styles.bigField}>
 					<div className={styles.title}>Message.</div>
-					<textarea name="message" className={styles.input + " " + styles.largeModule} style={{ backgroundColor: colors.background }}></textarea>
+					<textarea name="message" className={styles.input + " " + styles.largeModule} style={{ backgroundColor: colors.background }} required></textarea>
 				</div>
 			</form>
 			<div
