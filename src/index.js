b47986ecb4d9c51d52be2a1423dc3a1427b5b34d
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2024-02-28 10:08:35 +0100

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -4,8 +4,23 @@ import "./index.scss"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
+const injectGA = () => {
+	if (typeof window == "undefined") {
+		return
+	}
+	window.dataLayer = window.dataLayer || []
+	function gtag() {
+		window.dataLayer.push(arguments)
+	}
+	gtag("js", new Date())
+
+	gtag("config", "G-07JGETDCYE")
+}
+
 ReactDOM.render(
 	<React.StrictMode>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_TRACKING_ID" />
+		<script>{injectGA()}</script>
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
